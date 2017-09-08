b2ca89a94db1499c8d1145aea581cb6b29d80ceb
Jarvan <liushileijarvan@gmail.com>
2017-09-08 22:51:04 +0800

    p

--- HelloWorld/src/index.js
+++ HelloWorld/src/index.js
@@ -10,8 +10,6 @@ app.model(require("./models/users"));
 // 2. Plugins
 app.use(createLoading());
 // 3. Model
-// app.model(require('./models/example'));
-// app.model(require('./models/users'))
 
 // 4. Router
 app.router(require('./router'));
